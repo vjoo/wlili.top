0bcfbafe01522821579d4e9c4db81251d6b9df2b
vjoo <vjoo@163.com>
2026-07-15 19:30:05 +0800

    2

--- tools/nav.js
+++ tools/nav.js
@@ -17,6 +17,7 @@
     { name: '在线换算', href: './unit-converter.html' },
     { name: '密码生成器', href: './password-gen.html' },
     { name: 'IP形象生成器', href: './ip-mascot.html' },
+    { name: '3D模型预览', href: './model-viewer.html' },
     { name: '3D耗材管理', href: './filament-manager.html' },
     { name: '音标速查', href: './phonetic-chart.html' },
     { name: '打印管理器', href: './print-manager.html' }
